2dcf074185530639243f6ac4f329f38c3a84320e
Stacey Sugiono <sugiono.stacey@gmail.com>
2018-10-16 12:26:46 -0700

    adding jshint settings

--- server.js
+++ server.js
@@ -13,7 +13,45 @@ app.use(morgan('common'));
 app.use(express.static('public'));
 
 
-app.listen(process.env.PORT || 8080, () => console.log(
-	`Your app is listening on port ${process.env.PORT || 8080}`));
+let server;
 
-module.exports = app;
+function runServer(databaseUrl, port=PORT) {
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, err => {
+			if (err) {
+				return reject(err);
+			}
+
+			server = app.listen(port, () => {
+				console.log(`Your app is listening on port ${port}`);
+				resolve();
+			})
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err);
+			});
+		});
+	});
+}
+
+function closeServer() {
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
+}
+
+if (require.main === module) {
+	runServer(DATABASE_URL).catch(err => console.error(err));
+}
+// app.listen(process.env.PORT || 8080, () => console.log(
+// 	`Your app is listening on port ${process.env.PORT || 8080}`));
+
+module.exports = { runServer, app, closeServer }; 
