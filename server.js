1edd3537b8c86c0505b6aa993bb57258b53548c2
Stacey Sugiono <sugiono.stacey@gmail.com>
2018-10-15 22:32:48 -0700

    added test for root url

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 'use strict'
 
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('common'));
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 8080, () => console.log(
+	`Your app is listening on port ${process.env.PORT || 8080}`));
+
+module.exports = { app };
