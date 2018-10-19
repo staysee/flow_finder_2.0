16c6c0c366e409b0e3178f0e42b84603b1e1dbd2
Stacey Sugiono <sugiono.stacey@gmail.com>
2018-10-19 10:30:14 -0700

    created addressString virtual

--- models.js
+++ models.js
@@ -13,8 +13,26 @@ const eventSchema = mongoose.Schema({
 		zipcode: String
 	},
 	date: Date,
+	time: {
+		startTime: Number,
+		endTime: Number
+	},
 	prop: String
 })
+
+eventSchema.virtual('addressString').get(function() {
+	return `${this.address.building} ${this.address.street}`.trim();
+})
+
+eventSchema.methods.serialize = function() {
+	return {
+		id: this._id,
+		name: this.name,
+		description: this.description,
+		address: this.addressString,
+		prop: this.prop
+	};
+}
 
 const Event = mongoose.model('Event', eventSchema);
 module.exports = { Event };
