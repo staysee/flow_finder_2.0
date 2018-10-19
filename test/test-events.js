3dab6fadbe313b146671c97bb886faab8c29ec3a
Stacey Sugiono <sugiono.stacey@gmail.com>
2018-10-19 10:53:07 -0700

    create fake data for testing

--- test/test-events.js
+++ test/test-events.js
@@ -2,6 +2,7 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const faker = require('faker');
 const mongoose = require('mongoose');
 
 const expect = chai.expect;
@@ -13,5 +14,57 @@ const { TEST_DATABASE_URL } = require('../config');
 chai.use(chaiHttp);
 
 
+function seedEventData() {
+	console.info('seeding event data');
+	const eventData = [];
+
+	for(let i=1; i<=10; i++){
+		seedData.push(generateEventData());
+	}
+	return Event.insertMany(seedData);
+}
+
+function generateProp() {
+	const props = ['Hoop', 'Staff', 'Poi', 'Rope Dart'];
+	return props[Math.floor(Math.random() * props.length)];
+}
+
+function generateEventData(){
+	return {
+		name: faker.lorem.sentence(),
+		description: faker.lorem.paragraph(),
+		address:{
+			building: faker.address.streetAddress(),
+			street: faker.address.streetName(),
+			city: faker.address.city(),
+			state: faker.address.state(),
+			zipcode: faker.address.zipcode()
+		},
+		date: 'January 1, 2018',
+		time: {
+			startTime: 7,
+			endTime: 9
+		},
+		prop: generateProp()
+	}
+}
 
 
+
+const eventSchema = mongoose.Schema({
+	name: {type: String, required: true},
+	description: {type: String, required: true},
+	address: {
+		building: String,
+		street: String,
+		city: String,
+		state: String,
+		zipcode: String
+	},
+	date: Date,
+	time: {
+		startTime: Number,
+		endTime: Number
+	},
+	prop: String
+})
